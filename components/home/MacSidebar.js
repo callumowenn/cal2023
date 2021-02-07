8cb62d2325efde48f1048d7cad7f11c1ec78b3d0
Callum Owen <callumowen@outlook.com>
2021-02-07 16:07:53 +0000

    fixed safari blur 🧭

--- components/home/MacSidebar.js
+++ components/home/MacSidebar.js
@@ -4,6 +4,7 @@ function MacSidebar({ children }) {
 			className="flex flex-col p-3 w-130px sm:w-90vw sm:h-12"
 			style={{
 				backdropFilter: "blur(50px)",
+				WebkitBackdropFilter: "blur(50px)",
 				background: "#11111180",
 			}}
 		>
