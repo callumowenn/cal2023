3fb0f5f9c8917f6bfaabb99ca0e6490ff0d7f857
Callum Owen <callumowen@outlook.com>
2021-02-09 14:40:15 +0000

    project content colour fix 🎨

--- components/projects/ProjectContent.js
+++ components/projects/ProjectContent.js
@@ -2,6 +2,13 @@ function ProjectContent({ content }) {
 	return (
 		<div className="project-content flex flex-col self-center max-w-750px">
 			{content}
+			<p className="hidden text-standout-green"></p>
+			<p className="hidden  text-standout-pink"></p>
+			<p className="hidden text-standout-purple"></p>
+			<p className="hidden text-standout-blue"></p>
+			<p className="hidden text-standout-red"></p>
+			<p className="hidden text-standout-yellow"></p>
+			<p className="hidden text-standout-orange"></p>
 		</div>
 	);
 }
