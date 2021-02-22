fa45da34d8a4f5970098059e0972838f91a45b24
Callum Owen <callumowen@outlook.com>
2021-02-22 11:28:19 +0000

    blur tweaks

--- components/BlurUpImage.js
+++ components/BlurUpImage.js
@@ -1,25 +1,43 @@
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Blurhash } from "react-blurhash";
 
 function BlurUpImage({ src, blurhash, width, height, layout, classes }) {
 	const [loaded, setLoaded] = useState(false);
+
+	const loadImageWithPromise = (src) =>
+		new Promise((resolve) => {
+			const image = document.createElement("img");
+			image.onload = resolve;
+			image.src = src;
+		});
+
+	const awaitImage = async () => {
+		try {
+			await loadImageWithPromise(src);
+			setLoaded(true);
+		} catch {
+			console.error(`Unable to load ${src}`);
+		}
+	};
+
+	useEffect(() => {
+		awaitImage();
+	});
+
 	return (
 		<>
 			<Image
 				className={classes + ` absolute top-0`}
-				style={{ transition: "0.5s" }}
 				src={src}
+				priority
 				width={width}
 				layout={layout}
 				height={height}
-				onLoad={() => {
-					setLoaded(true);
-				}}
 			/>
 			<div
 				className={`absolute top-0 ${loaded ? " opacity-0" : ""}`}
-				style={{ transition: "1s" }}
+				style={{ transition: "0.5s" }}
 			>
 				<Blurhash
 					hash={blurhash}
